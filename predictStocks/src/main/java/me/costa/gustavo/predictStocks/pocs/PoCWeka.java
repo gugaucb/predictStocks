8fd513c6974645eb4be3bf6c70b5a93f173ac7c2
Gustavo Luís da Costa <gugaucb@gmail.com>
2017-03-17 06:43:58 -0300

    PoCWeka Funcionando

--- predictStocks/src/main/java/me/costa/gustavo/predictStocks/pocs/PoCWeka.java
+++ predictStocks/src/main/java/me/costa/gustavo/predictStocks/pocs/PoCWeka.java
@@ -30,78 +30,126 @@ public class PoCWeka {
 		try {
 			// Reading training arff or csv file
 			Instances train = carregarDataTrain(filepath);
-			 carregarModelo();
-			 
-			 MultilayerPerceptron mlp = criarModelo();
+			 MultilayerPerceptron mlp;
+			Instances filteredData = filtrarInstancesTrain(train);
+			try{ 
+				mlp = carregarModelo();
+			}catch(Exception e){
+				mlp = criarModelo();
+
+				buildClassifier(filteredData, mlp);
+				salvarModelo(mlp);			
+			}
+
 			
 			
-			Instances filteredData = filtrarInstances(train);
-			System.out.println(" iniciando buildClassifier");
-			mlp.buildClassifier(filteredData);
-			System.out.println(" fim buildClassifier");
-			
-			 /** Another Way to set parameters, Where, L = Learning Rate M =
+
+			/* *//** Another Way to set parameters, Where, L = Learning Rate M =
 			 * Momentum N = Training Time or Epochs H = Hidden Layers etc.
-			 */
-			mlp.setOptions(Utils.splitOptions("-L 0.1 -M 0.2 -N 2000 -V 0 -S 0 -E 20 -H 6"));
+			 *//*
+			mlp.setOptions(Utils.splitOptions("-L 0.1 -M 0.2 -N 2000 -V 0 -S 0 -E 20 -H 6"));*/
 
 			
 			/* * Neural Classifier Training Validation For evaluation of training
 			 * data,*/
 			 
-			System.out.println(" iniciando Evaluation");
-			Evaluation eval = new Evaluation(train);
-			eval.evaluateModel(mlp, train);
-			System.out.println(eval.errorRate()); // Printing Training Mean root
-													// squared Error
-			System.out.println(eval.toSummaryString()); // Summary of Training
-			System.out.println(" fim Evaluation");
+			/*Evaluation eval = executarEvaluation(mlp, filteredData);
+			salvarModelo(mlp);
 			// To apply K-Fold validation
-			int kfolds = 10;
-			System.out.println(" iniciando crossValidateModel");
-			eval.crossValidateModel(mlp, train, kfolds, new Random(1));
-			System.out.println(" fim crossValidateModel");
+			executarCrossValidateModel(mlp, filteredData, eval);
+			salvarModelo(mlp);*/
 			// Evaluating/Predicting unlabelled data
-			Instances test = new Instances(new BufferedReader(new FileReader(PoCWeka.class.getResource("testDataSetTweet.arff").getPath())));
+			Instances test = carregarDataTest();
 			
 			
-			StringToWordVector filterTest = new StringToWordVector();
-			 // Initialize filter and tell it about the input format.
-			test.setClassIndex(test.numAttributes()-1);
-			filterTest.setInputFormat(test);
-			filterTest.setMinTermFreq(10);
-			filterTest.setOutputWordCounts(true);
-			filterTest.setWordsToKeep(1000000);
-			filterTest.setNormalizeDocLength(new SelectedTag(StringToWordVector.FILTER_NORMALIZE_ALL, StringToWordVector.TAGS_FILTER));
-			// Generate word counts from the training data.
-			Instances datapredict = Filter.useFilter(test, filterTest);
+			Instances datapredict = filtrarTestInstances(test);
 			
 			
 
 			
 			
 			
-			System.out.println(" iniciando predicteddata");
-			datapredict.setClassIndex(datapredict.numAttributes() - 1);
-			Instances predicteddata = new Instances(datapredict);
-			// Predict Part
-			for (int i = 0; i < datapredict.numInstances(); i++) {
-				double clsLabel = mlp.classifyInstance(datapredict.instance(i));
-				predicteddata.instance(i).setClassValue(clsLabel);
-			}
-			// Storing again in arff
-			BufferedWriter writer = new BufferedWriter(new FileWriter("text.arff"));
-			writer.write(predicteddata.toString());
-			writer.newLine();
-			writer.flush();
-			writer.close();
+			classificarDataTest(mlp, datapredict);
 
-			weka.core.SerializationHelper.write("multilayerPerceptron"+System.currentTimeMillis()+".model", mlp);
+			salvarModelo(mlp);
 			
 			System.out.println(" fim predicteddata");
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+	}
+
+	private static Instances buildClassifier(Instances train, MultilayerPerceptron mlp) throws Exception {
+		Instances filteredData = filtrarInstancesTrain(train);
+		mlp.buildClassifier(filteredData);
+		System.out.println(" fim buildClassifier");
+		return filteredData;
+	}
+
+	private static Instances filtrarInstancesTrain(Instances train) throws Exception {
+		Instances filteredData = filtrarInstances(train);
+		System.out.println(" iniciando buildClassifier");
+		return filteredData;
+	}
+
+	private static void salvarModelo(MultilayerPerceptron mlp) throws Exception {
+		weka.core.SerializationHelper.write("multilayerPerceptron"+System.currentTimeMillis()+".model", mlp);
+	}
+
+	private static void classificarDataTest(MultilayerPerceptron mlp, Instances datapredict)
+			throws Exception, IOException {
+		System.out.println(" iniciando predicteddata");
+		datapredict.setClassIndex(datapredict.numAttributes() - 1);
+		Instances predicteddata = new Instances(datapredict);
+		// Predict Part
+		for (int i = 0; i < datapredict.numInstances(); i++) {
+			double clsLabel = mlp.classifyInstance(datapredict.instance(i));
+			predicteddata.instance(i).setClassValue(clsLabel);
+		}
+		// Storing again in arff
+		BufferedWriter writer = new BufferedWriter(new FileWriter("text.arff"));
+		writer.write(predicteddata.toString());
+		writer.newLine();
+		writer.flush();
+		writer.close();
+	}
+
+	private static Instances filtrarTestInstances(Instances test) throws Exception {
+		StringToWordVector filterTest = new StringToWordVector();
+		 // Initialize filter and tell it about the input format.
+		test.setClassIndex(test.numAttributes()-1);
+		filterTest.setInputFormat(test);
+		filterTest.setMinTermFreq(10);
+		filterTest.setOutputWordCounts(true);
+		filterTest.setWordsToKeep(1000000);
+		filterTest.setNormalizeDocLength(new SelectedTag(StringToWordVector.FILTER_NORMALIZE_ALL, StringToWordVector.TAGS_FILTER));
+		// Generate word counts from the training data.
+		Instances datapredict = Filter.useFilter(test, filterTest);
+		return datapredict;
+	}
+
+	private static Instances carregarDataTest() throws IOException, FileNotFoundException {
+		Instances test = new Instances(new BufferedReader(new FileReader(PoCWeka.class.getClassLoader().getResource("testDataSetTweet.arff").getPath())));
+		return test;
+	}
+
+	private static void executarCrossValidateModel(MultilayerPerceptron mlp, Instances filteredData, Evaluation eval)
+			throws Exception {
+		int kfolds = 10;
+		System.out.println(" iniciando crossValidateModel");
+		eval.crossValidateModel(mlp, filteredData, kfolds, new Random(1));
+		System.out.println(" fim crossValidateModel");
+	}
+
+	private static Evaluation executarEvaluation(MultilayerPerceptron mlp, Instances filteredData) throws Exception {
+		System.out.println(" iniciando Evaluation");
+		Evaluation eval = new Evaluation(filteredData);
+		eval.evaluateModel(mlp, filteredData);
+		System.out.println(eval.errorRate()); // Printing Training Mean root
+												// squared Error
+		System.out.println(eval.toSummaryString()); // Summary of Training
+		System.out.println(" fim Evaluation");
+		return eval;
 	}
 
 	private static Instances carregarDataTrain(String filepath) throws FileNotFoundException, IOException {
@@ -127,8 +175,8 @@ public class PoCWeka {
 		return filteredData;
 	}
 
-	private static void carregarModelo() throws Exception {
-		MultilayerPerceptron mlp = (MultilayerPerceptron) weka.core.SerializationHelper.read(PoCWeka.class.getClassLoader().getResource("multilayerPerceptron1489686696365.model").getPath());
+	private static MultilayerPerceptron carregarModelo() throws Exception {
+		return (MultilayerPerceptron) weka.core.SerializationHelper.read(PoCWeka.class.getClassLoader().getResource("multilayerPerceptron1489711727528.model").getPath());
 	}
 
 	private static MultilayerPerceptron criarModelo() {
